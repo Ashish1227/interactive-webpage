9afb845165cef35b049f2ad44213309824e9888c
Ashish1227 <ashishgatsy@gmail.com>
2024-12-26 19:37:51 +0530

    final smol change lmao

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -5,7 +5,7 @@ const ResultScreen = () => {
     <div className="flex flex-col items-center justify-center text-center p-4">
       <h1 className="text-3xl font-bold text-pink-700 mb-4">Thank You Stuuuu! ❤️</h1>
       <p className="text-lg text-gray-600">
-        You’ve made my day. I’m so excited to begin this journey with you!
+        You’ve made my day. Also I mishhooo
       </p>
     </div>
   );
